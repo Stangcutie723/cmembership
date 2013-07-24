f006a916a172a3bf872a5941cead61521758cb49
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-24 14:30:46 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cmembership::Application.routes.draw do
   resources :memberships
+  root to: "memberships#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -41,7 +42,7 @@ Cmembership::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
